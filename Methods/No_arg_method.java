db0ebfec572bb13cdecb2beb15237f1ecd6b2be0
Sahil Patane <patanesahil@gmail.com>
2025-04-07 21:09:29 +0530

    No argument Method

---
+++ Methods/No_arg_method.java
@@ -0,0 +1,19 @@
+class No_arg_method{
+  public static void main(String [] args)
+  {
+    System.out.println("Main Start");
+    demo();
+    test();
+    System.out.println("Main End");
+  }
+  public static void demo()
+  {
+    System.out.println("Demo Start");
+    System.out.println("Demo End");
+  }
+  public static void test()
+  {
+    System.out.println("Test Start");
+    System.out.println("Test End");
+  }
+}
