b0dc9381261a8cb9876066510dce9ff1968b12ea
Sahil Patane <patanesahil@gmail.com>
2025-04-07 15:53:28 +0530

    For loop statement

---
+++ Flow Control Statements/For.java
@@ -0,0 +1,11 @@
+class For{
+	public static void main(String[] args) {
+		System.out.println("Main Start");
+		int a;
+		for(a=1;a<=7;a++)
+		{
+			System.out.println("S1");
+		}
+		System.out.println("Main End");
+	}
+}
